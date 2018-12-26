7de095a58d8b233714e6de4017281222b2eaed8f
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-12-26 00:50:46 -0500

    Created new controller for locales

---
+++ app/controllers/locales_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class LocalesController < ApplicationController
+  def change
+    return head_bad_request_and_log_error unless I18n.available_locales.include?(params[:locale].to_sym)
+
+    session[:locale] = params[:locale]
+    logger.info "* Locale was changed to #{params[:locale]}"
+  end
+
+  private
+
+  def head_bad_request_and_log_error
+    head :bad_request
+    logger.error "[X] Invalid locale #{params[:locale]}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root to: 'static_pages#index'
+
+  post 'locales/change', to: 'locales#change', as: :change_locale
 end
