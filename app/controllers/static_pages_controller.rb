b5b42d7c12127824a2251a506dc8a0f8400d0f6c
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2019-01-07 07:39:30 -0500

    Refactoring views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,9 +4,10 @@ class StaticPagesController < ApplicationController
   skip_before_action :user_logged_in?, only: :index
   skip_before_action :user_confirmed?, only: :index
 
-  layout false
-
   def index
-    render :index, layout: 'application_with_transparent_header'
+    @body_gradient_background = true
+    @transparent = true
+
+    render :index
   end
 end
