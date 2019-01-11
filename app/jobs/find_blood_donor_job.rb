816f30e534729292d676ea6363431c01974f8cef
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2019-01-10 23:26:05 -0500

    Added new methods for the job

--- app/jobs/find_blood_donor_job.rb
+++ app/jobs/find_blood_donor_job.rb
@@ -3,6 +3,34 @@
 class FindBloodDonorJob < TwilioJob
   queue_as :find_donor
 
-  def perform
+  def perform(user, amount)
+    blood_types = find_compatible_blood_types(user.blood_type)
+    donors = find_available_donors(blood_types, amount)
+    notify_and_donors_status(donors)
+    update_donor_status(donor)
+  end
+
+  private
+
+  def find_compatible_blood_types(blood_type)
+  end
+
+  def find_available_donors(blood_types, amount)
+  end
+
+  def notify_and_donors_status(donors)
+    donors.each do |donor|
+      next unless notify_donor(donor)
+
+      update_donor_status(donor)
+    end
+  end
+
+  def notify_donor(donor)
+    message = 'You have been matched'
+    send_sms_message(donor.phone, message)
+  end
+
+  def update_donor_status(donor)
   end
 end
