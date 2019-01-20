1bbd07d5ef72c291f1a4ce050c7bbbb70b411d61
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2019-01-19 22:42:09 -0500

    Added new methods for BloodType model

--- app/models/blood_type.rb
+++ app/models/blood_type.rb
@@ -2,4 +2,13 @@
 
 class BloodType < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  has_many :donors, through: :donors_id
+
+  def can_receive_from
+    BloodCompatibility.where(receiver_id: self.id).map(&:donator)
+  end
+
+  def can_donate_to
+    BloodCompatibility.where(donator_id: self.id).map(&:receiver)
+  end
 end
