e954052288391b88c4f977923a09311ca4f5a6a6
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2019-02-09 18:55:13 -0500

    Fixing tests

--- spec/models/blood_donation_request_spec.rb
+++ spec/models/blood_donation_request_spec.rb
@@ -7,19 +7,24 @@ RSpec.describe BloodDonationRequest, type: :model do
     let(:user) { FactoryBot.create(:user) }
 
     it 'is valid with valid attributes' do
-      expect(BloodDonationRequest.new(user: user, status: 'Matching in progress')).to be_valid
+      expect(BloodDonationRequest.new(user: user, status: 'Matching in progress', amount: 1)).to be_valid
     end
 
     it 'is invalid without a user' do
-      expect(BloodDonationRequest.new(status: 'Matching in progress')).not_to be_valid
+      expect(BloodDonationRequest.new(status: 'Matching in progress', amount: 1)).not_to be_valid
     end
 
     it 'is invalid without a status' do
-      expect(BloodDonationRequest.new(user: user)).not_to be_valid
+      expect(BloodDonationRequest.new(user: user, amount: 1)).not_to be_valid
     end
 
     it 'is invalid with invalid status' do
-      expect(BloodDonationRequest.new(status: 'Invalid status')).not_to be_valid
+      expect(BloodDonationRequest.new(user: user, status: 'Invalid status', amount: 1)).not_to be_valid
+    end
+
+    it 'is invalid with a below zero amount' do
+      expect(BloodDonationRequest.new(user: user, status: 'Matching in progress', amount: 0)).not_to be_valid
+      expect(BloodDonationRequest.new(user: user, status: 'Matching in progress', amount: -1)).not_to be_valid
     end
   end
 end
