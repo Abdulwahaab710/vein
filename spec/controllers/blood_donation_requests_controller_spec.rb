b7f809c7b5fc3f162e59f79b16c92e754e1f2522
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2019-02-09 15:47:43 -0500

    Added missing tests

--- spec/controllers/blood_donation_requests_controller_spec.rb
+++ spec/controllers/blood_donation_requests_controller_spec.rb
@@ -57,4 +57,24 @@ RSpec.describe BloodDonationRequestsController, type: :controller do
         .to change { BloodDonationRequest.count }.from(0).to(1)
     end
   end
+
+  describe 'DELETE #destroy' do
+    before :each do
+      user = FactoryBot.create(:user, phone_confirmed: true)
+      FactoryBot.create(:session, user: user)
+      @blood_donation_request = FactoryBot.create(:blood_donation_request, user: user, amount: 2)
+
+      login_as user
+    end
+
+    it 'returns success' do
+      delete :destroy, params: { id: @blood_donation_request.id }
+      expect(response).to have_http_status(:no_content)
+    end
+
+    it 'creates a blood donation request' do
+      expect{ delete :destroy, params: { id: @blood_donation_request.id } }
+        .to change { BloodDonationRequest.where(status: BloodDonationRequestStatus::WITHDRAWN).count }.from(0).to(1)
+    end
+  end
 end
